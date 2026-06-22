9ca7322b9e9d2ce8ed5f16c8c23e639048f39dbf
Bruno Oliveiram <bruno.o.s@live.com>
2026-06-22 16:09:56 -0300

    Adicionando o Spring Security ao Projeto

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,13 +1,12 @@
 package com.devsuperior.dscommerce;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DscommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DscommerceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DscommerceApplication.class, args);
+    }
 
 }
